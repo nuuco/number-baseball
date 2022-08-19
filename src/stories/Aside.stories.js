8e64ac2c842dca26cfa1a22988afea90a94dc31a
nuuco <nuuco.coding@gmail.com>
2022-08-19 10:28:57 +0900

    feat: Aside 스토리 파일 작성

--- src/stories/Aside.stories.js
+++ src/stories/Aside.stories.js
@@ -1,10 +1,17 @@
-import { Aside } from "../components/Aside"
+import {Aside} from "../components/Aside"
+
+const mockupData =  [
+	"123 ⚾ 1B",
+	"345 ⚾ 1S",
+	"654 ❌ Out",
+  ];
 
 export default {
 	title: "Practice/Aside",
 	component: Aside,
 	argTypes: {
-		title: { control: "text" }
+		scoreRecord: [],
+		setScoreRecord: () => {}
 	}
 }
 
@@ -12,3 +19,15 @@ const Template = (args) => <Aside {...args} />
 
 
 export const NomalAside = Template.bind({});
+export const EmptyAside = Template.bind({});
+
+NomalAside.args = {
+	scoreRecord: mockupData,
+	setScoreRecord: () => {}
+}
+
+EmptyAside.args = {
+	scoreRecord: [],
+	setScoreRecord: () => {}
+}
+
