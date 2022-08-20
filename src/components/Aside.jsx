21973193dc6f5a2fa1821d01b7a039d1cf408b94
nuuco <nuuco.coding@gmail.com>
2022-08-20 10:18:56 +0900

    style: Aside 스타일드 컴포넌트로 스타일링

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -3,7 +3,65 @@ import styled from "styled-components";
 import colorSheet from "../colorSheet";
 
 const ScoreBoard = styled.div`
+display: flex;
+flex-direction: column;
+font-size: 18px;
+font-weight: 500;
+flex-basis: 20%;
+min-width: 200px;
 
+    h2 {
+        font-size: 16px;
+        margin-bottom: 6px;
+        color: ${colorSheet.scoreBoardTitle};
+    }
+    section {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background-color: ${colorSheet.scoreBoardBg};
+        border: 2px solid ${colorSheet.scoreBoardBorder};
+        border-radius: 10px;
+    }
+
+    ul {
+        flex: 1 0 0;
+        overflow: auto;
+        padding: 20px 23px 0 23px;
+        margin-bottom: 10px;
+        line-height: 1.6;
+        overflow: auto;
+    }
+
+    li:last-child {
+        margin-bottom: 50px;
+    }
+
+    .clear-btn {
+        display: block;
+        width: 70%;
+        height: 40px;
+        margin: 20px auto;
+        border: 2px solid ${colorSheet.mainColor};
+        background-color: transparent;
+        border-radius: 30px;
+        color: ${colorSheet.mainColor};
+        font-size: 16px;
+        font-weight: 700;
+        cursor: pointer;
+    }
+
+    .clear-btn:hover {
+        background-color: ${colorSheet.mainColor};
+        color: ${colorSheet.whiteFontColor};
+    }
+
+    .clear-btn.invalid {
+        background-color: ${colorSheet.scoreBoardTitle};
+        border-color: ${colorSheet.scoreBoardTitle};
+        color: ${colorSheet.whiteFontColor};
+    }
 `;
 
 
@@ -20,7 +78,7 @@ export const Aside = ({scoreRecord, setScoreRecord}) => {
                     <li key={idx}>{el}</li>
                 ))}
                 </ul>
-                <button onClick={handleClearBtn}>Clear</button>
+                <button className={`clear-btn ${scoreRecord.length === 0 ? "invalid" : ""}`} onClick={handleClearBtn}>Clear</button>
             </section>   
         </ScoreBoard>
     )
